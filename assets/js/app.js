dcd89b6feb4066532f3f591544950b79f08e8813
Pauliih <pau.campos89@gmail.com>
2017-12-14 16:42:18 -0300

    back + +flags + 10 dig + code aleatorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,68 @@
 $(function() {
   
   $('#signup').hide();
-  $('#main').hide();
+  $('#form-signup').hide();
+  $('#verify-phone').hide();
   
   setInterval(function() {
     $('#splash').fadeOut(500);
-    //permite el scroll de nuevo
     $('#signup').show();
   }, 1000);
    
-  //Revisar eventos
-  $('.btn-signup').click(function(){
-  	$('#signup').hide();
-  	$('#main').show();
+  $('.btn-signup').click(function() {
+  	$('#signup').addClass('hidden-xs');
+  	$('#form-signup').removeClass('hidden-xs');
+    $('#form-signup').show();
   });
+
+  $('.back').click(function() {
+    $('#form-signup').addClass('hidden-xs');
+    $('#verify-phone').addClass('hidden-xs');
+    $('#signup').removeClass('hidden-xs');
+    //Al volver se limpia el phone number
+    $('#phone').val("");
+
+    //Deshabilito el boton next
+    $('.btn-next').attr('disabled','disabled');
+
+  });
+/*
+  $('.back2').click(function() {
+    $('#form-signup').addClass('hidden-xs');
+    $('#verify-phone').removeClass('hidden-xs');
+  });
+*/
+
+  //Habilita el boton next al ingresar 10 digitos
+  $('#demo').keyup(function() {
+    var num = $(this).val();
+    for (var i = 0; i < 1; i++) {
+      if (num.length == 10) {
+        $('.btn-next').removeAttr('disabled');
+      }else{
+        $('.btn-next').attr('disabled','disabled');
+      };
+    };
+  });
+
+  $('.btn-next').click(function() {
+    var code = "";
+    var str = "0123456789";
+    for (var i = 0; i < 3; i++) {
+      code += str.charAt(Math.floor(Math.random() * str.length));
+    }
+    alert("Tu código es: LAB-" + code); // Alerta se corta*
+  });
+
+  $('.btn-next').click(function() {
+    $('#form-signup').hide();
+    $('#verify-phone').removeClass('hidden-xs');
+    $('#verify-phone').show();
+  });
+
+  //banderas
+  $("#demo").intlTelInput();
+
+  
 });
+
